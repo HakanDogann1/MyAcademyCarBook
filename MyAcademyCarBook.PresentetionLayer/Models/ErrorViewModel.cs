de76fdd89f718b8b2182a037adeb8d66859a58f7
Hakan <dgnhakan1@gmail.com>
2023-09-28 21:58:24 +0300

    Add project files.

---
+++ MyAcademyCarBook.PresentetionLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyAcademyCarBook.PresentetionLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
