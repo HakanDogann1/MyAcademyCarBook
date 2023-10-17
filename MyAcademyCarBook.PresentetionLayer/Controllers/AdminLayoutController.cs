7a8227ed692d1c33fc8dbe87d32b87297cd35a8b
Hakan <dgnhakan1@gmail.com>
2023-10-17 21:11:55 +0300

    17.10.2023

---
+++ MyAcademyCarBook.PresentetionLayer/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAcademyCarBook.PresentetionLayer.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MyAcademyCarBook.PresentetionLayer/Controllers/BrandController.cs
+++ MyAcademyCarBook.PresentetionLayer/Controllers/BrandController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyAcademyCarBook.BusinessLayer.Abstract;
+using MyAcademyCarBook.EntityLayer.Concrete;
 
 namespace MyAcademyCarBook.PresentetionLayer.Controllers
 {
@@ -16,6 +17,35 @@ namespace MyAcademyCarBook.PresentetionLayer.Controllers
         {
            var values = _brandService.TGetAll();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddBrand()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddBrand(Brand brand)
+        {
+            brand.Status = true;
+            _brandService.TAdd(brand);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteBrand(int id)
+        {
+            _brandService.TRemove(id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateBrand(int id)
+        {
+            var value = _brandService.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateBrand(Brand brand)
+        {
+            _brandService.TUpdate(brand);
+            return RedirectToAction("Index");
         }
     }
 }
